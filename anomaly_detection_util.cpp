972919cbf72f7ff5b389e8c9af0887623f442315
osher <oshereli2@walla.com>
2021-10-14 18:39:38 +0300

    Add the 3 files of ex1

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,48 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: write your ID and name here
+ */
+
+#include <math.h>
+#include "anomaly_detection_util.h"
+
+float avg(float* x, int size){
+	return 0;
+}
+
+// returns the variance of X and Y
+float var(float* x, int size){
+	return 0;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+	return 0;
+}
+
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+	return 0;
+}
+
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size){
+
+	return Line(0,0);
+}
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+	return 0;
+}
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+	return 0;
+}
+
+
+
+
