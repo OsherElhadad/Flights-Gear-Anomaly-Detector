61a912213736a16bce6c9b71ccd9f195b99f9b4c
yossi <yossix07@gmail.com>
2021-12-15 21:23:36 +0200

    starting ex5 - add: CLI.cpp CLI.h commands.h

---
+++ CLI.cpp
@@ -0,0 +1,17 @@
+/*
+ * CLI.cpp
+ *
+ * Author: Yossi Maatook, 208641472
+ * Author: Osher Elhadad, 318969748
+ */
+#include "CLI.h"
+
+CLI::CLI(DefaultIO* dio) {
+}
+
+void CLI::start(){
+}
+
+
+CLI::~CLI() {
+}
---
+++ CLI.h
@@ -0,0 +1,24 @@
+/*
+ * CLI.h
+ *
+ * Author: Yossi Maatook, 208641472
+ * Author: Osher Elhadad, 318969748
+ */
+#ifndef CLI_H_
+#define CLI_H_
+
+#include <string.h>
+#include "commands.h"
+
+using namespace std;
+
+class CLI {
+    DefaultIO* dio;
+    // you can add data members
+public:
+    CLI(DefaultIO* dio);
+    void start();
+    virtual ~CLI();
+};
+
+#endif /* CLI_H_ */
